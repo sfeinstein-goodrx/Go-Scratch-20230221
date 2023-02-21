18276d4a264f25291a0cdf207bfb03ee6132b833
Scott Feinstein <sfeinstein@goodrx.com>
2023-02-21 13:36:59 -0600

    refactor: decompose gitHubInfo a bit more

--- githubInfo_from_api/githubInfo.go
+++ githubInfo_from_api/githubInfo.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	url2 "net/url"
 )
 
 func main() {
@@ -16,16 +18,21 @@ func main() {
 }
 
 func githubInfo(login string) (string, int, error) {
-	url := "https://api.github.com/users/" + login
+	//url := "https://api.github.com/users/" + login
+	url := fmt.Sprintf("https://api.github.com/users/%s", url2.QueryEscape(login))
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", 0, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return "", 0, err
+		return "", 0, fmt.Errorf("%q: bad status - %s", url, resp.Status)
 	}
+	return parse(resp.Body)
+}
+
+func parse(rdr io.Reader) (string, int, error) {
 	var user gitHubUser
-	userDecoder := json.NewDecoder(resp.Body)
+	userDecoder := json.NewDecoder(rdr)
 	if err := userDecoder.Decode(&user); err != nil {
 		return "", 0, err
 	}
