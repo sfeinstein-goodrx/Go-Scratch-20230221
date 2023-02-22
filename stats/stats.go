5e696a59d7b01b4a8d2693b518f07d6110d015ea
Scott Feinstein <sfeinstein@goodrx.com>
2023-02-22 13:54:07 -0600

    feat: more on stats and generics

--- stats/stats.go
+++ stats/stats.go
@@ -13,13 +13,20 @@ func main() {
 	fmt.Println(MaxFloat64s(fs))
 	fmt.Println(Max(fs))
 	fmt.Println(Max[float64](nil))
+
+	hs := []Height{2, 3, 1}
+	fmt.Println(Max(hs))
 }
 
 // Max
 // square brackets for "type constraints"
-func Max[T int | float64](values []T) (T, error) {
+// orig:
+// func Max[T int | float64](values []T) (T, error) {
+func Max[T Ordered](values []T) (T, error) {
 	if len(values) == 0 {
-		return 0, fmt.Errorf("Max on empty slice")
+		var zero T // helper for just this situation with string, tells us the default zero value for a type
+		return zero, fmt.Errorf("Max on empty slice")
+		//return 0, fmt.Errorf("Max on empty slice")
 	}
 	m := values[0]
 	for _, v := range values[1:] {
@@ -29,6 +36,13 @@ func Max[T int | float64](values []T) (T, error) {
 	}
 	return m, nil
 }
+
+type Height float64
+type Ordered interface {
+	//int | float64 | string
+	~int | ~float64 | ~string // tilda tells us it isnt exactly that type but any type that is underneath the same
+}
+
 func MaxInts(values []int) (int, error) {
 	if len(values) == 0 {
 		return 0, fmt.Errorf("MaxInts on empty slice")
